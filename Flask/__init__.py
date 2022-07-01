8205dbee2623a799338e8ab3be41433c785059d1
Abell-Choi <trooperalex92@gmail.com>
2022-07-01 14:26:57 +0900

    add some data

--- Flask/__init__.py
+++ Flask/__init__.py
@@ -1,6 +1,9 @@
+from operator import methodcaller
+from unittest import result
 from flask import Flask, jsonify, request, json, request_tearing_down, redirect
 import json
 import requests
+from Module.ArcaconDownloader import ArcaconUtility
 
 app = Flask(__name__)
 
@@ -13,13 +16,33 @@ def _resultType(strRes, objValue):
     }
 
 
-# 트위치 # 스트리머 목록 받아오기
-@app.route('/api/v1/getArcaconContents', methods=['POST'])
-def getCachedStreammerList():
+
+# 아카콘 존재 여부 확인
+# POST strData
+@app.route('/api/v1/findArcaconContents', methods=['POST'])
+def findArcaconContents():
     objPostData = request.values.to_dict()
+    if not 'strData' in objPostData.keys():
+        return json.dumps(_resultType('err', 'no strData in POST data'), ensure_ascii=False)
+    
+    if not type(objPostData['strData']) is str:
+        return json.dumps(_resultType('err', 'strData must be String type'), ensure_ascii=False)
+    
+    obj = ArcaconUtility()
+    return json.dumps(obj.findCodeForContent(objPostData['strData']), ensure_ascii=False)
+
+@app.route('/api/v1/getArcaconContents', methods=['POST'])
+def getArcaconContents():
+    objPostData = request.values.to_dict()
+    if not 'strData' in objPostData.keys():
+        return json.dumps(_resultType('err', 'no strData in POST data'), ensure_ascii=False)
+
+    if not type(objPostData['strData']) is str:
+        return json.dumps(_resultType('err', 'strData must be String type'), ensure_ascii=False)
+    
+    obj = ArcaconUtility()
+    return json.dumps(obj.getContentDataUrls(objPostData['strData']), ensure_ascii=False)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
-    #ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-    #ssl_context.load_cert_chain(certfile='server.crt', keyfile='server.key', password='PURURU966a#@!')
-    #app.run(host='0.0.0.0', port=8080, debug=True)
--- Flutter/lib/Pages/root_page.dart
+++ Flutter/lib/Pages/root_page.dart
@@ -1,8 +1,15 @@
 
+import 'dart:developer';
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:temp/Pages/Utility/card.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+Future<http.Response> fetchPost() {
+  return http.get(Uri.parse('www.naver.com'));
+}
 
 runApplication() => {
       runApp(MaterialApp(
@@ -26,11 +33,96 @@ class _RootPageState extends State<RootPage> {
     width: MediaQuery.of(context).size.width/3*2,
   );
 
-  int _checkUrlData(String url){
-    return 0;
+  Future<String> getParse(String url, String strData) async{
+    var getData = await http.post(
+      Uri.parse(url),
+      headers: <String, String> { 'Content-Type': 'application/x-www-form-urlencoded',},
+      body: <String, String> { 'strData' : strData }
+    );
+    return getData.body;
   }
 
+//실패시
+void errorDialog() {
+  showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          // RoundedRectangleBorder - Dialog 화면 모서리 둥글게 조절
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0)),
+          //Dialog Main Title
+          title: Column(
+            children: <Widget>[
+              new Text("에러"),
+            ],
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "해당 데이터를 찾을 수 없습니다.",
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            new TextButton(
+              child: new Text("확인"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      });
+  }
 
+  // 작업 성공시 다일로그
+  void resDialog(String title, String targetData) async {
+    var getRes = await getParse(
+      'http://127.0.0.1:8080/api/v1/getArcaconContents', 
+      targetData
+    );
+
+    Map<String, dynamic> data = jsonDecode(getRes);
+
+    print(data['value'][0]);
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          // RoundedRectangleBorder - Dialog 화면 모서리 둥글게 조절
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0)),
+          //Dialog Main Title
+          title: Column(
+            children: <Widget>[
+              new Text(title),
+            ],
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Image.network(
+                data['value'][0] 
+              ),
+            ]
+          ),
+          actions: <Widget>[
+            new TextButton(
+              child: new Text("확인"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,8 +143,20 @@ class _RootPageState extends State<RootPage> {
             ),
             constructedContainer(
               ElevatedButton(
-                onPressed: (){
-                  print(urlController.text);
+                onPressed: ()async{
+                  Map<String, dynamic> jsonData = jsonDecode (await getParse(
+                    'http://127.0.0.1:8080/api/v1/findArcaconContents', 
+                    urlController.text
+                    ));
+
+                  // 임시 에러처리
+                  if (jsonData['res'] == 'err'){ 
+                    print('err');
+                    errorDialog();
+                    return;
+                  }
+
+                  resDialog(jsonData['value'], urlController.text);
                 }, 
                 child: Text('Search')
               )
