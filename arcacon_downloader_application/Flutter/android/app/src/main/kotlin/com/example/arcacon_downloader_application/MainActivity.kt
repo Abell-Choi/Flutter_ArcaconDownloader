d73962f197f91063354bbc59f7385612393909b5
Abell_Choi <trooperalex92@gmail.com>
2022-07-22 16:56:38 +0900

    Delete arcacon_downloader_application directory

--- arcacon_downloader_application/Flutter/android/app/src/main/kotlin/com/example/arcacon_downloader_application/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.arcacon_downloader_application
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- arcacon_downloader_application/Flutter/lib/widgets/download_tile.dart
+++
@@ -1,9 +0,0 @@
-
-import 'package:flutter/material.dart';
-
-Widget downloadTile(){
-  ListTile root = ListTile(
-    leading: null,
-  );
-  return Container();
-}
